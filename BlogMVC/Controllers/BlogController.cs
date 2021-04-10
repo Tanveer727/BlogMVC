96aefad1186b2f8ffb6a0d55c2963fb9064a83d9
Tanveer <Tanveer@LAPTOP-P9L2N7CU>
2021-04-10 11:59:21 +0530

    Add project files.

---
+++ BlogMVC/Controllers/BlogController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogMVC.Controllers
+{
+    public class BlogController : Controller
+    {
+        BlogEntities blogEntities = new BlogEntities();
+        // GET: Blog
+        public ActionResult Index()
+        {
+            var blogdetails = blogEntities.Blogs.ToList().OrderByDescending(x=>x.Bid);
+            return View(blogdetails);
+        }
+
+
+        public ActionResult UploadBlog()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadBlog(Blog bg)
+        {           
+            blogEntities.Blogs.Add(bg);
+            blogEntities.SaveChanges();
+            ViewBag.message = "Blog Details Are saved Successfully..!";
+            return View();
+        }
+
+
+        public ActionResult Food()
+        {
+            var Foodarticle = blogEntities.Blogs.Where(x=>x.BCategory == "Food").OrderByDescending(x => x.Bid);
+            return View(Foodarticle);
+
+        }
+
+
+        public ActionResult Sports()
+        {
+            var Sportsarticle = blogEntities.Blogs.Where(x => x.BCategory == "Sports").OrderByDescending(x => x.Bid);
+            return View(Sportsarticle);
+
+        }
+
+
+        public ActionResult Movies()
+        {
+            var Sportsarticle = blogEntities.Blogs.Where(x => x.BCategory == "Movies").OrderByDescending(x => x.Bid);
+            return View(Sportsarticle);
+
+        }
+
+        public ActionResult Recipesworld()
+        {
+            return View();
+        }
+
+        public ActionResult HindiMovies()
+        {
+            return View();
+        }
+
+        public ActionResult CricketTips()
+        {
+            return View();
+                 
+        }
+        
+      
+    }
+}
